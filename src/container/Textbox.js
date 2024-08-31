cb8138f784776a0984c20a9c1799a415f34a9dc2
Amanrathi07 <amanrathi4321@gmail.com>
2024-08-30 22:49:39 -0700

    use first state and make a button which convert all text to uppercase

--- src/container/Textbox.js
+++ src/container/Textbox.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 export default function Textbox() {
-  return (
-    <>
 
-    
+  const[text,setText]=useState('');
+
+  const handelOnChange=(event)=>{
+    setText(event.target.value);
+  }
+  const hendelUpClicked=function hendelUpClicked(){
+    let newText=text.toUpperCase() ;
+    setText(newText);
+  }
+
+  return (
+    <>    
   <div className="container">
     <h3 className="p-3">Text Area</h3>
-    <textarea className="col-sm-11" rows="13" ></textarea>
+    <textarea placeholder='Enter your text' value={text} className="col-sm-11" rows="13" onChange={handelOnChange} ></textarea>
 
   <div>    
-    <button onClick={()=>{console.log("btn was clicked");}} className="mt-5 btn btn-primary ">convert to upper</button>
+    <button onClick={hendelUpClicked}  className="mt-5 btn btn-primary ">convert to upper</button>
   </div>
     
   </div>
-
-   
-    
-    
-   
-    
+      
     </>
   )
 }
