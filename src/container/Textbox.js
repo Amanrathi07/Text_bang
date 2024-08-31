6d92cbbd08d0442b34e090e5e193170291aca7d1
Amanrathi07 <amanrathi4321@gmail.com>
2024-08-30 21:51:29 -0700

    add button and rearanging it

--- src/container/Textbox.js
+++ src/container/Textbox.js
@@ -9,7 +9,9 @@ export default function Textbox() {
     <h3 className="p-3">Text Area</h3>
     <textarea className="col-sm-11" rows="13" ></textarea>
 
-  
+  <div>    
+    <button onClick={()=>{console.log("btn was clicked");}} className="mt-5 btn btn-primary ">convert to upper</button>
+  </div>
     
   </div>
 
