66d18449ab3d5cfdb615b7e7124ab89b64500b91
Amanrathi07 <amanrathi4321@gmail.com>
2024-09-02 06:56:30 +0530

    add button for lower case

--- src/container/Textbox.js
+++ src/container/Textbox.js
@@ -12,6 +12,11 @@ export default function Textbox() {
     setText(newText);
   }
 
+  const hendelDownClicked=function hendelUpClicked(){
+    let newText=text.toLowerCase() ;
+    setText(newText);
+  }
+
   return (
     <>    
   <div className="container">
@@ -20,6 +25,7 @@ export default function Textbox() {
 
   <div>    
     <button onClick={hendelUpClicked}  className="mt-4 btn btn-primary ">convert to upper</button>
+    <button onClick={hendelDownClicked}  className="mx-2 mt-4 btn btn-primary ">convert to lower</button>
   </div>
     
   </div>
